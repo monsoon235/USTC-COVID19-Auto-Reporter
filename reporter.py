f31f518dcc700e50a0117e296f21f6bae97086d2
monsoon <yjh1021317464@gmail.com>
2020-09-28 05:33:21 +0800

    update default_info, split personal_info to info.yaml

--- reporter.py
+++ reporter.py
@@ -1,19 +1,10 @@
 import sys
 import time
-from typing import Optional
+from typing import List, Optional
 
 import requests
+import yaml
 from bs4 import BeautifulSoup
-
-person_list = [
-    {
-        'id': 'PBxxxxxxxx',  # 学号，必选
-        'password': 'xxxxxxxx',  # 密码，必选
-        'now_province': 'xx0000',  # 所在省份行政代号，如江苏省为 320000，可选，如没有这条则默认为安徽
-        'now_city': 'xxxx00',  # 所在城市行政代号，如江苏南京为 320100，可选，如没有这条则默认为合肥
-    },
-    # ...
-]
 
 default_info = {
     'now_address': '1',  # 当前所在地 1:内地 2:香港 3:国外 4:澳门 5:台湾
@@ -30,17 +21,9 @@ default_info = {
     'now_status_detail': '',  # 具体情况，当前状态选择“其他”时填写
     'has_fever': '0',  # 目前有无发热症状 0:无 1: 有
     'last_touch_sars': '0',  # 是否接触过疑似患者  0:无 1:有
-    'last_touch_sars_date': '',
-    'last_touch_sars_detail': '',
-    'last_touch_hubei': '0',
-    'last_touch_hubei_date': '',
-    'last_touch_hubei_detail': '',
-    'last_cross_hubei': '0',
-    'last_cross_hubei_date': '',
-    'last_cross_hubei_detail': '',
-    'return_dest': '1',
-    'return_dest_detail': '',
-    'other_detail': ''
+    'last_touch_sars_date': '',  # 最近一次接触日期，当是否接触过疑似患者为“有”时填写
+    'last_touch_sars_detail': '',  # 具体情况，当是否接触过疑似患者为“有”时填写
+    'other_detail': ''  # 其他情况说明
 }
 
 headers = {
@@ -60,16 +43,17 @@ headers = {
     'sec-fetch-site': 'same-origin',
     'sec-fetch-user': '?1',
     'upgrade-insecure-requests': '1',
-    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36'
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36'
 }
 
 
-def get_full_data(person: dict) -> dict:
-    info = default_info.copy()
-    p = person.copy()
-    p.pop('id')
-    p.pop('password')
-    info.update(p)
+def read_info() -> List[dict]:
+    with open('info.yaml') as f:
+        info = yaml.load(f, Loader=yaml.FullLoader)
+    for i in range(len(info)):
+        full = default_info.copy()
+        full.update(info[i])
+        info[i] = full
     return info
 
 
@@ -98,17 +82,20 @@ def check_report(sess: requests.Session) -> bool:
     if info is None:
         return False
     info = info.text
-    if info.find('上次上报时间：2020-') is None:
+    if info.find('上次上报时间：') is None:
         return False
     start = info.find('：') + 1
-    date_str = info[start:start + len('2020-00-00')]
+    date_str = info[start:start + len('0000-00-00')]
     if time.strftime("%Y-%m-%d", time.localtime()) == date_str:
         return True
     return False
 
 
-def report(person: dict, check_first: bool = True) -> bool:
-    sess = login(person['id'], person['password'])
+def report(info: dict, check_first: bool = True) -> bool:
+    data = info.copy()
+    sess = login(data['id'], data['password'])
+    data.pop('id')
+    data.pop('password')
     if sess is None:
         return False
     if check_first and check_report(sess):
@@ -118,7 +105,6 @@ def report(person: dict, check_first: bool = True) -> bool:
     token = soup.find('input', {'name': '_token'})['value']
     if token is None:
         return False
-    data = get_full_data(person)
     data['_token'] = token
     url = 'http://weixine.ustc.edu.cn/2020/daliy_report'
     headers['cookie'] = \
@@ -133,12 +119,12 @@ def report(person: dict, check_first: bool = True) -> bool:
 
 
 if __name__ == '__main__':
-    for person in person_list:
+    for info in read_info():
         try:
-            success = report(person, check_first=False)
+            success = report(info, check_first=False)
             if success:
-                print(f"{person['id']} report successfully")
+                print(f"{info['id']} report successfully")
             else:
-                print(f"{person['id']} report failed")
+                print(f"{info['id']} report failed")
         except Exception as e:
             sys.stderr.write(str(e) + '\n')
