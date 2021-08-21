19c0ba72fdf2751b53760486d5b0ba2fdbc4ce03
monsoon <yjh1021317464@gmail.com>
2021-08-21 09:45:53 +0800

    Use CST timezone in run.sh

--- run.sh
+++ run.sh
@@ -4,6 +4,7 @@ apt install -y tesseract-ocr tesseract-ocr-eng locales
 sed -i 's/# zh_CN.UTF-8 UTF-8/zh_CN.UTF-8 UTF-8/g' /etc/locale.gen
 locale-gen
 export LANG=zh_CN.UTF-8
+ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
 
 pip install -i https://mirrors.ustc.edu.cn/pypi/web/simple -r requirements.txt
 
