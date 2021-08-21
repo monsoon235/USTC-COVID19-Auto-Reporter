d5718e9a8d024f01f6b6c5e48c9ea527fd63e10c
monsoon <yjh1021317464@gmail.com>
2021-08-21 09:39:36 +0800

    Fix run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 sed -i 's/deb.debian.org/mirrors.ustc.edu.cn/g' /etc/apt/sources.list
 apt update
-apt install -y tesseract-ocr tesseract-ocr-eng locale
+apt install -y tesseract-ocr tesseract-ocr-eng locales
 sed -i 's/# zh_CN.UTF-8 UTF-8/zh_CN.UTF-8 UTF-8/g' /etc/locale.gen
 export LANG=zh_CN.UTF-8
 
